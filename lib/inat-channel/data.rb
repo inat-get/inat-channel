01a210523ccec70743f23a03fee2c82952c18d5c
Иван Шихалев <shikhalev@gmail.com>
2025-12-04 04:42:38 +0500

    data.rb

--- lib/inat-channel/data.rb
+++ lib/inat-channel/data.rb
@@ -1,16 +1,75 @@
+require 'json'
+require 'fileutils'
 
 module INatChannel
+  
+  def pool_file
+    config[:pool_file] || 'pool.json'
+  end
+
+  def sent_file
+    config[:sent_file] || 'sent.json'
+  end
 
   def pool
-    # TODO: implement lazy loading
+    @pool ||= load_pool
   end
 
   def sent
-    # TODO: implement lazy loading
+    @sent ||= load_sent
   end
 
   def save
-    # TODO: implement saving both lists: pool and sent
+    save_pool
+    save_sent
+    logger.info "Saved pool=#{pool.size}, sent=#{sent.size}"
+  end
+
+  def add_to_pool(new_uuids)
+    new_pool = (pool + new_uuids).uniq - sent.keys
+    @pool = new_pool
+    logger.info "Added #{new_uuids.size} new UUIDs to pool (total: #{pool.size})"
+  end
+
+  def pop_random
+    return nil if pool.empty?
+
+    uuid = pool.sample
+    pool.delete(uuid)
+
+    sent[uuid] = nil
+    logger.debug "Popped random UUID: #{uuid}"
+    uuid
+  end
+
+  private
+
+  def load_pool
+    return [] unless File.exist?(pool_file)
+
+    JSON.parse(File.read(pool_file), symbolize_names: true)
+  rescue => e
+    logger.warn "Failed to load pool from #{pool_file}: #{e.message}. Starting empty."
+    []
+  end
+
+  def load_sent
+    return {} unless File.exist?(sent_file)
+
+    JSON.parse(File.read(sent_file), symbolize_names: true)
+  rescue => e
+    logger.warn "Failed to load sent from #{sent_file}: #{e.message}. Starting empty."
+    {}
+  end
+
+  def save_pool
+    FileUtils.mkdir_p(File.dirname(pool_file))
+    File.write(pool_file, JSON.pretty_generate(pool))
+  end
+
+  def save_sent
+    FileUtils.mkdir_p(File.dirname(sent_file))
+    File.write(sent_file, JSON.pretty_generate(sent))
   end
 
 end
